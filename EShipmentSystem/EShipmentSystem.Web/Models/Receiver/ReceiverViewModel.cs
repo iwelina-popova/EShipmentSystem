3296f194b30cf3df348c50c403c126600e792197
iwelina-popova <iwelina.popova@gmail.com>
2017-01-10 17:18:54 +0200

    Add view models

---
+++ EShipmentSystem/EShipmentSystem.Web/Models/Receiver/ReceiverViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace EShipmentSystem.Web.Models.Receiver
+{
+    public class ReceiverViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Address { get; set; }
+
+        public string Neighborhood { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string OptionalInfo { get; set; }
+    }
+}
---
+++ EShipmentSystem/EShipmentSystem.Web/Models/Shipment/ShipmentViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace EShipmentSystem.Web.Models.Shipment
+{
+    using System.Collections.Generic;
+    using AdditionalOptions;
+    using Receiver;
+    using User;
+
+    public class ShipmentViewModel
+    {
+        public int Id { get; set; }
+
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public double Weight { get; set; }
+
+        public UserVIewModel User { get; set; }
+
+        public ReceiverViewModel Receiver { get; set; }
+
+        public IEnumerable<AdditionalOptionsViewModel> Options { get; set; }
+    }
+}
